2ef364c8709729f2dc3e8857360fe710c057dcaf
sunao <sunao_0626@hotmail.com>
2017-10-28 23:47:44 +0800

    !!!!

--- hw1/hw1_rnn.sh
+++ hw1/hw1_rnn.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
-
-#!/usr/bin/env bash
 if [ ! -d "./models/" ]; then
-    wget -O rnn.zip "https://www.dropbox.com/s/1cy7h6urtoy4po0/rnn.rar?dl=1"
-    unzip rnn.zip
+    wget -O models.zip "https://www.dropbox.com/s/ha9ih272gzjb9x9/models.zip?dl=1"
+    unzip models.zip
 fi
 
 python model_rnn.py -a 4 -dp $1 -tp $2 -mp ./models/rnn/
