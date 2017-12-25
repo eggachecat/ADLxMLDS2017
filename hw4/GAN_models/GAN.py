0a2f80fd8dad7d1955f22c5cdb7a3249ef1f8ac7
sunao <sunao_0626@hotmail.com>
2017-12-25 11:10:04 +0800

    23333

--- hw4/GAN_models/GAN.py
+++ hw4/GAN_models/GAN.py
@@ -120,6 +120,129 @@ class GAN:
         self.generator = None
         self.discriminator = None
 
+        self.D_loss, self.G_loss, \
+        self.D_acc_r, self.D_acc_r_t1, \
+        self.D_acc_r_t2, self.D_acc_G = self.make_loss(
+            self.discriminator.R_prob,
+            self.discriminator.R_prob_t1,
+            self.discriminator.R_prob_t2,
+            self.discriminator.G_prob)
+
+        self.D_train, self.G_train = self.make_train(self.D_loss, self.G_loss)
+
+    def make_loss(self, R_prob, R_prob_t1, R_prob_t2, G_prob):
+        D_loss_r = tf.reduce_mean(
+            tf.nn.sigmoid_cross_entropy_with_logits(logits=R_prob, labels=tf.ones_like(R_prob),
+                                                    name="D_loss_real_XE_1"), 1, name="D_loss_real")
+
+        D_loss_r_t1 = tf.reduce_mean(
+            tf.nn.sigmoid_cross_entropy_with_logits(logits=R_prob_t1, labels=tf.ones_like(R_prob_t1),
+                                                    name="D_loss_real_t1_XE_1"), 1, name="D_loss_real_t1")
+
+        D_loss_r_t2 = tf.reduce_mean(
+            tf.nn.sigmoid_cross_entropy_with_logits(logits=R_prob_t2, labels=tf.ones_like(R_prob_t2),
+                                                    name="D_loss_real_t2_XE_1"), 1, name="D_loss_real_t2")
+        D_loss_G = tf.reduce_mean(
+            tf.nn.sigmoid_cross_entropy_with_logits(logits=G_prob, labels=tf.zeros_like(G_prob),
+                                                    name="D_loss_fake_XE_0"), 1, name="D_loss_fake")
+
+        D_loss = D_loss_r + D_loss_r_t1 + D_loss_r_t2 + D_loss_G
+
+        D_acc_r = tf.reduce_mean(tf.cast(tf.greater(R_prob, 0.5 * tf.ones_like(R_prob)), tf.float32))
+        D_acc_r_t1 = tf.reduce_mean(tf.cast(tf.greater(R_prob_t1, 0.5 * tf.ones_like(R_prob_t1)), tf.float32))
+        D_acc_r_t2 = tf.reduce_mean(tf.cast(tf.greater(R_prob_t2, 0.5 * tf.ones_like(R_prob_t2)), tf.float32))
+
+        D_acc_G = tf.reduce_mean(tf.cast(tf.less(G_prob, 0.5 * tf.ones_like(G_prob)), tf.float32))
+
+        G_loss = tf.reduce_mean(
+            tf.nn.sigmoid_cross_entropy_with_logits(logits=G_prob, labels=tf.ones_like(G_prob), name="G_loss_XE_0"), 1,
+            name="D_loss_G")
+
+        return D_loss, G_loss, D_acc_r, D_acc_r_t1, D_acc_r_t2, D_acc_G
+
+    def make_train(self, D_loss, G_loss):
+        D_optimizer = self.settings["Discriminator"]["optimizer"]["type"]
+        D_learning_rate = self.settings["Discriminator"]["optimizer"]["learning_rate"]
+        D_train = D_optimizer(D_learning_rate).minimize(
+            D_loss, var_list=tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope="Discriminator"))
+
+        G_optimizer = self.settings["Generator"]["optimizer"]["type"]
+        G_learning_rate = self.settings["Generator"]["optimizer"]["learning_rate"]
+        G_train = G_optimizer(G_learning_rate).minimize(
+            D_loss, var_list=tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope="Generator"))
+
+        return D_train, G_train
+
+
+class WGAN:
+    def __init__(self, settings, generator, discriminator):
+        self.generator = generator
+        self.discriminator = discriminator
+
+        self.settings = settings
+        self.generator = None
+        self.discriminator = None
+
+        self.D_loss, self.G_loss, self.D_acc_r, self.D_acc_r_t1, self.D_acc_r_t2, self.D_acc_G = self.make_loss(
+            self.discriminator.R_prob,
+            self.discriminator.R_prob_t1,
+            self.discriminator.R_prob_t2,
+            self.discriminator.G_prob)
+        self.D_train, self.G_train = self.make_train(self.D_loss, self.G_loss)
+
+    def make_loss(self, R_prob, R_prob_t1, R_prob_t2, G_prob):
+        D_loss_r = tf.reduce_mean(
+            tf.nn.sigmoid_cross_entropy_with_logits(logits=R_prob, labels=tf.ones_like(R_prob),
+                                                    name="D_loss_real_XE_1"), 1, name="D_loss_real")
+
+        D_loss_r_t1 = tf.reduce_mean(
+            tf.nn.sigmoid_cross_entropy_with_logits(logits=R_prob_t1, labels=tf.ones_like(R_prob_t1),
+                                                    name="D_loss_real_t1_XE_1"), 1, name="D_loss_real_t1")
+
+        D_loss_r_t2 = tf.reduce_mean(
+            tf.nn.sigmoid_cross_entropy_with_logits(logits=R_prob_t2, labels=tf.ones_like(R_prob_t2),
+                                                    name="D_loss_real_t2_XE_1"), 1, name="D_loss_real_t2")
+        D_loss_G = tf.reduce_mean(
+            tf.nn.sigmoid_cross_entropy_with_logits(logits=G_prob, labels=tf.zeros_like(G_prob),
+                                                    name="D_loss_fake_XE_0"), 1, name="D_loss_fake")
+
+        D_loss = D_loss_r + D_loss_r_t1 + D_loss_r_t2 + D_loss_G
+
+        D_acc_r = tf.reduce_mean(tf.cast(tf.greater(R_prob, 0.5 * tf.ones_like(R_prob)), tf.float32))
+        D_acc_r_t1 = tf.reduce_mean(tf.cast(tf.greater(R_prob_t1, 0.5 * tf.ones_like(R_prob_t1)), tf.float32))
+        D_acc_r_t2 = tf.reduce_mean(tf.cast(tf.greater(R_prob_t2, 0.5 * tf.ones_like(R_prob_t2)), tf.float32))
+
+        D_acc_G = tf.reduce_mean(tf.cast(tf.less(G_prob, 0.5 * tf.ones_like(G_prob)), tf.float32))
+
+        G_loss = tf.reduce_mean(
+            tf.nn.sigmoid_cross_entropy_with_logits(logits=G_prob, labels=tf.ones_like(G_prob), name="G_loss_XE_0"), 1,
+            name="D_loss_G")
+
+        return D_loss, G_loss, D_acc_r, D_acc_r_t1, D_acc_r_t2, D_acc_G
+
+    def make_train(self, D_loss, G_loss):
+        D_optimizer = self.settings["Discriminator"]["optimizer"]["type"]
+        D_learning_rate = self.settings["Discriminator"]["optimizer"]["learning_rate"]
+        D_train = D_optimizer(D_learning_rate).minimize(
+            D_loss, var_list=tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope="Discriminator"))
+
+        G_optimizer = self.settings["Generator"]["optimizer"]["type"]
+        G_learning_rate = self.settings["Generator"]["optimizer"]["learning_rate"]
+        G_train = G_optimizer(G_learning_rate).minimize(
+            D_loss, var_list=tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope="Generator"))
+
+        return D_train, G_train
+
+
+class ImprovedGAN:
+    def __init__(self, settings, generator, discriminator):
+        self.generator = generator
+        self.discriminator = discriminator
+
+        self.settings = settings
+        self.generator = None
+        self.discriminator = None
+
         self.D_loss, self.G_loss, self.D_acc_r, self.D_acc_r_t1, self.D_acc_r_t2, self.D_acc_G = self.make_loss(
             self.discriminator.R_prob,
             self.discriminator.R_prob_t1,
--- hw4/main.py
+++ hw4/main.py
@@ -1,11 +1,18 @@
 from GAN_models.GAN import *
 from experiment_settings import simple_settings
 import tensorflow as tf
+import tensorflow.contrib.data as tf_data_api
 import json
 import skimage
 import skimage.io
 import skimage.transform
 import scipy.misc
+
+import numpy as np
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import LabelBinarizer
+
+np.random.seed(0)
 
 tf.set_random_seed(0)
 
@@ -16,19 +23,22 @@ n_G_train = 1
 
 n_data = 0
 
+GLOBAL_HAIRS = ['orange', 'white', 'aqua', 'gray',
+                'green', 'red', 'purple', 'pink',
+                'blue', 'black', 'brown', 'blonde']
+
+GLOBAL_EYES = ['gray', 'black', 'orange', 'pink',
+               'yellow', 'aqua', 'purple', 'green',
+               'brown', 'red', 'blue']
+
 
 def preprocessing(data_path):
     print("start loading....")
-    with open(data_path, "r") as fp:
+    with open("{}/tags_clean.csv".format(data_path), "r") as fp:
         lines = fp.readlines()
 
-    hairs = ['orange hair', 'white hair', 'aqua hair', 'gray hair',
-             'green hair', 'red hair', 'purple hair', 'pink hair',
-             'blue hair', 'black hair', 'brown hair', 'blonde hair']
-
-    eyes = ['gray eyes', 'black eyes', 'orange eyes', 'pink eyes',
-            'yellow eyes', 'aqua eyes', 'purple eyes', 'green eyes',
-            'brown eyes', 'red eyes', 'blue eyes']
+    hairs = [hair + " hair" for hair in GLOBAL_HAIRS]
+    eyes = [eye + " eyes" for eye in GLOBAL_EYES]
 
     data_obj = dict()
     for line in lines:
@@ -41,24 +51,26 @@ def preprocessing(data_path):
             if keyword in eyes:
                 tags["eyes"] = keyword.split(" ")[0]
 
-        if "hairs" in tags or "eyes" in tags:
+        if "hair" in tags and "eyes" in tags:
             data_obj[id_] = tags
 
     with open("./tags.json", "w") as fp:
         json.dump(data_obj, fp)
-    for id in data_obj:
-        img = skimage.io.imread("d:/workstation/adl/data/hw4/faces/{}.jpg".format(id))
+
+    for _id in data_obj:
+        img = skimage.io.imread("{}/faces/{}.jpg".format(data_path, _id))
         img_resized = skimage.transform.resize(img, (64, 64), mode='constant')
-        scipy.misc.imsave("d:/workstation/adl/data/hw4/faces_resized/{}.jpg".format(id), img_resized)
+        scipy.misc.imsave("{}/faces_resized/{}.jpg".format(data_path, _id), img_resized)
 
     img_resized_list = []
-    for id in data_obj:
-        img_resized_list.append(skimage.io.imread("d:/workstation/adl/data/hw4/faces_resized/{}.jpg".format(id)))
+    id_list = []
+    for _id in data_obj:
+        img_resized_list.append(skimage.io.imread("{}/faces_resized/{}.jpg".format(data_path, _id)))
+        id_list.append(int(_id))
     np.save("data/img_list.npy", img_resized_list)
+    np.save("data/id_list.npy", id_list)
+
     return data_obj
-
-
-import numpy as np
 
 
 def main():
@@ -80,32 +92,6 @@ def main():
 
     generator = Generator(settings["generator"], z_sample, c_sample)
 
-    sess = tf.Session()
-    sess.run(tf.global_variables_initializer())
-
-    g_sample_ = sess.run(generator.G_sample, feed_dict={
-        z_sample: np.array([
-            [[[1, 2, 3]]],
-            [[[4, 5, 6]]],
-            [[[7, 8, 9]]],
-            [[[17, 18, 19]]],
-            [[[71, 81, 91]]]
-
-        ]),
-        c_sample: np.array([
-            [[[0, 0, 0, 0, 1]]],
-            [[[0, 0, 0, 1, 0]]],
-            [[[0, 0, 1, 0, 0]]],
-            [[[0, 1, 0, 0, 0]]],
-            [[[1, 0, 0, 0, 0]]],
-        ])
-    })
-
-    print(g_sample_.shape)
-    for i in range(g_sample_.shape[0]):
-        scipy.misc.imsave("./early/{}.jpg".format(i), g_sample_[i])
-
-    exit()
     discriminator = Discriminator(settings["discriminator"], g_sample, r_sample, c_sample, r_sample_neg, c_sample_neg,
                                   condition_dim)
 
@@ -114,57 +100,194 @@ def main():
     sess = tf.Session()
     sess.run(tf.global_variables_initializer())
 
-    g_sample_ = sess.run(generator.G_sample, feed_dict={
-        z_sample: np.array([
-            [[[1, 2, 3]]],
-            [[[4, 5, 6]]]
-        ]),
-        c_sample: np.array([
-            [[[0, 0, 1]]],
-            [[[1, 0, 0]]]
-        ])
-    })
-    g_prob, r_prob, r_prob_t1, r_prob_t2 = sess.run(
-        [discriminator.G_prob, discriminator.R_prob, discriminator.R_prob_t1, discriminator.R_prob_t2], feed_dict={
-            g_sample: g_sample_,
-            r_sample: g_sample_,
-            c_sample: np.array([
-                [[[0, 0, 1]]],
-                [[[1, 0, 0]]]
-            ]),
-            c_sample_neg: np.array([
-                [[[0, 0, 1]]],
-                [[[1, 0, 0]]]
-            ]),
-            r_sample_neg: g_sample_
-        })
+    images = np.load("./data/img_list.npy")
+    ids = np.load("./data/id_list.npy")
 
-    print(g_prob, r_prob, r_prob_t1, r_prob_t2)
+    with open("./tags.json") as fp:
+        _tags_obj = json.load(fp)
+
+    ctr = 0
+    tags_obj = dict()
+    for key in _tags_obj.keys():
+        tags_obj[int(key)] = _tags_obj[key]
+        if "hair" in _tags_obj[key] and "eyes" in _tags_obj[key]:
+            ctr += 1
+
+    all_ids = tags_obj.keys()
+
+    dataset = tf_data_api.Dataset.from_tensor_slices((images, ids))
+    dataset = dataset.batch(batch_size)
+    iterator = dataset.make_initializable_iterator()
+    next_element = iterator.get_next()
+
+    sess = tf.Session()
+
+    for _ in range(1):
+        sess.run(iterator.initializer)
+        while True:
+            try:
+                images_, ids_ = sess.run(next_element)
+                conditions = ids2conditions(tags_obj, ids_)
+
+                helper_conditions = []
+                helper_ids = []
+                for _ in range(batch_size):
+                    _help_id = np.random.choice(all_ids, 1)
+                    _condition = ids2conditions(tags_obj, _help_id)[0]
+                    if any(np.equal(np.array(conditions), _condition).all(1)):
+                        helper_conditions.append(_condition)
+                        helper_ids.append(_help_id[0])
+
+                helper_images = [images[_id] for _id in helper_ids]
+                helper_conditions = np.array(helper_conditions)
+
+                g_sample_ = sess.run(generator.G_sample, feed_dict={
+                    z_sample: np.random.rand(batch_size, z_dim),
+                    c_sample: conditions
+                })
+
+                g_prob, r_prob, r_prob_t1, r_prob_t2 = sess.run(
+                    [discriminator.G_prob, discriminator.R_prob, discriminator.R_prob_t1, discriminator.R_prob_t2],
+                    feed_dict={
+                        g_sample: g_sample_,
+                        r_sample: images,
+                        c_sample: conditions,
+                        c_sample_neg: helper_conditions,
+                        r_sample_neg: helper_images
+                    })
+
+                sess.run([gan.D_train, gan.G_train])
+            except tf.errors.OutOfRangeError:
+                break
 
 
+global_hairs_encoder = LabelBinarizer()
+global_eyes_encoder = LabelBinarizer()
+global_hairs_encoder.fit(GLOBAL_HAIRS)
+global_eyes_encoder.fit(GLOBAL_EYES)
 
-    #
-    # for epoch in range(n_epoch):
-    #     for _ in range(n_data // batch_size):
-    #         pass
 
-    # inputs = sess.run(generator.inputs, feed_dict={
-    #     z_sample: np.array([
-    #         [[[1, 2, 3], [2, 3, 4], [1, 2, 3], [2, 3, 4]],
-    #          [[3, 4, 5], [4, 5, 6], [3, 4, 5], [4, 5, 6]],
-    #          [[5, 6, 7], [7, 8, 9], [5, 6, 7], [7, 8, 9]],
-    #          [[5, 6, 7], [7, 8, 9], [5, 6, 7], [7, 8, 9]]]
-    #     ]),
-    #     c_sample: np.array([
-    #         [[[1, 2, 3], [2, 3, 4], [1, 2, 3], [2, 3, 4]],
-    #          [[3, 4, 5], [4, 5, 6], [3, 4, 5], [4, 5, 6]],
-    #          [[5, 6, 7], [7, 8, 9], [5, 6, 7], [7, 8, 9]],
-    #          [[5, 6, 7], [7, 8, 9], [5, 6, 7], [7, 8, 9]]]
-    #     ])
-    # })
-    # print(inputs.shape, inputs)
+def ids2conditions(tags_obj, ids):
+    hair_code = global_hairs_encoder.transform([tags_obj[_id]["hair"] for _id in ids])
+    eyes_code = global_eyes_encoder.transform([tags_obj[_id]["eyes"] for _id in ids])
+    return np.concatenate((hair_code, eyes_code), axis=1)
 
 
 if __name__ == '__main__':
-    # preprocessing("d:/workstation/adl/data/hw4/tags_clean.csv")
+    # preprocessing("D:/workstation/adl/data/hw4")
     main()
+
+
+# discriminator = Discriminator(settings["discriminator"], g_sample, r_sample, c_sample, r_sample_neg, c_sample_neg,
+#                               condition_dim)
+#
+# gan = GAN(settings, generator, discriminator)
+#
+# sess = tf.Session()
+# sess.run(tf.global_variables_initializer())
+#
+# for n in range(n_epoch):
+#     pass
+#
+# exit()
+# g_sample_ = sess.run(generator.G_sample, feed_dict={
+#     z_sample: np.array([
+#         [[[1, 2, 3]]],
+#         [[[4, 5, 6]]],
+#         [[[7, 8, 9]]],
+#         [[[17, 18, 19]]],
+#         [[[71, 81, 91]]]
+#
+#     ]),
+#     c_sample: np.array([
+#         [[[0, 0, 0, 0, 1]]],
+#         [[[0, 0, 0, 1, 0]]],
+#         [[[0, 0, 1, 0, 0]]],
+#         [[[0, 1, 0, 0, 0]]],
+#         [[[1, 0, 0, 0, 0]]],
+#     ])
+# })
+#
+# print(g_sample_.shape)
+# for i in range(g_sample_.shape[0]):
+#     scipy.misc.imsave("./early/{}.jpg".format(i), g_sample_[i])
+#
+# exit()
+
+# exit()
+#
+# g_sample_ = sess.run(generator.G_sample, feed_dict={
+#     z_sample: np.array([
+#         [[[1, 2, 3]]],
+#         [[[4, 5, 6]]]
+#     ]),
+#     c_sample: np.array([
+#         [[[0, 0, 1]]],
+#         [[[1, 0, 0]]]
+#     ])
+# })
+# g_prob, r_prob, r_prob_t1, r_prob_t2 = sess.run(
+#     [discriminator.G_prob, discriminator.R_prob, discriminator.R_prob_t1, discriminator.R_prob_t2], feed_dict={
+#         g_sample: g_sample_,
+#         r_sample: g_sample_,
+#         c_sample: np.array([
+#             [[[0, 0, 1]]],
+#             [[[1, 0, 0]]]
+#         ]),
+#         c_sample_neg: np.array([
+#             [[[0, 0, 1]]],
+#             [[[1, 0, 0]]]
+#         ]),
+#         r_sample_neg: g_sample_
+#     })
+#
+# print(g_prob, r_prob, r_prob_t1, r_prob_t2)
+
+
+
+#
+# def _main():
+#     images = np.load("./data/img_list.npy")
+#     ids = np.load("./data/id_list.npy")
+#
+#     with open("./tags.json") as fp:
+#         _tags_obj = json.load(fp)
+#
+#     ctr = 0
+#     tags_obj = dict()
+#     for key in _tags_obj.keys():
+#         tags_obj[int(key)] = _tags_obj[key]
+#         if "hair" in _tags_obj[key] and "eyes" in _tags_obj[key]:
+#             ctr += 1
+#
+#     all_ids = tags_obj.keys()
+#
+#     dataset = tf_data_api.Dataset.from_tensor_slices((images, ids))
+#     dataset = dataset.batch(batch_size)
+#     iterator = dataset.make_initializable_iterator()
+#     next_element = iterator.get_next()
+#
+#     sess = tf.Session()
+#
+#     for _ in range(1):
+#         sess.run(iterator.initializer)
+#         while True:
+#             try:
+#                 images_, ids_ = sess.run(next_element)
+#                 conditions = ids2conditions(tags_obj, ids_)
+#
+#                 helper_conditions = []
+#                 helper_ids = []
+#                 for _ in range(batch_size):
+#                     _help_id = np.random.choice(all_ids, 1)
+#                     _condition = ids2conditions(tags_obj, _help_id)[0]
+#                     if any(np.equal(np.array(conditions), _condition).all(1)):
+#                         helper_conditions.append(_condition)
+#                         helper_ids.append(_help_id[0])
+#
+#                 helper_images = [images[_id] for _id in helper_ids]
+#                 helper_conditions = np.array(helper_conditions)
+#
+#
+#             except tf.errors.OutOfRangeError:
+#                 break
